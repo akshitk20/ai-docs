609aa4ea692d645a358dfb325d6c1f91e21e57b0
Akshit Khatri <akshitkhatri.20@gmail.com>
2024-07-28 17:25:08 +0530

    initial commit

---
+++ src/main/java/com/springboot/ai_docs/AiDocsApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.ai_docs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiDocsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiDocsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/ai_docs/AiDocsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.ai_docs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiDocsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
